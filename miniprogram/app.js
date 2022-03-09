641066d84da4a45f50569c4d9e5a7a7057a3e2aa
leejimqiu <leejimqiu@gmail.com>
2022-03-09 08:09:25 +0800

    feat: add update manager

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,26 @@
-// app.js
+
 App({
   onLaunch() {
     wx.cloud.init({
       env: ""
     });
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
   },
   globalData: {
     userInfo: null
