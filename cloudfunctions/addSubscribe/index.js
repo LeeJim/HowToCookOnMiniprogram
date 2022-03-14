201572481bf0dc5d2a3eac2760b96b3b0637cf56
leejimqiu <leejimqiu@gmail.com>
2022-03-15 00:08:10 +0800

    fix: storage correct subscribe data

--- cloudfunctions/addSubscribe/index.js
+++ cloudfunctions/addSubscribe/index.js
@@ -7,15 +7,23 @@ exports.main = async (event) => {
   const { FromUserName, CreateTime, MsgType, Event, List } = event
   // 消息订阅
   if (MsgType == 'event' && Event == 'subscribe_msg_popup_event') {
-    const list = Array.isArray(List) ? List.filter(item => item.SubscribeStatusString == 'accept') : List;
-    const { result } = await db.collection('subscribe').add({
-      data: {
-        status: 1,
-        list,
-        creator: FromUserName,
-        createTime: CreateTime,
-      }
-    })
-    return result
+    let list = []
+    if (Array.isArray(List)) {
+      list = List.filter(item => item.SubscribeStatusString == 'accept')
+    } else if (List.SubscribeStatusString == 'accept') {
+      list = [List]
+    }
+    
+    if (list.length > 0) {
+      const { result } = await db.collection('subscribe').add({
+        data: {
+          status: 1,
+          list,
+          creator: FromUserName,
+          createTime: CreateTime,
+        }
+      })
+      return result
+    }
   }
 }
