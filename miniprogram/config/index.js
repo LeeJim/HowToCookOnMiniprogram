87968553dcf3ac8b3416e70166298cae22c566fc
leejimqiu <leejimqiu@gmail.com>
2023-01-26 23:52:15 +0800

    fix: update title

--- miniprogram/config/index.js
+++ miniprogram/config/index.js
@@ -10,17 +10,17 @@ export const chineseMap = {
 }
 
 export const titleMap = {
-  breakfast: '早餐 Breakfast',
-  condiment: '佐料 Condiment',
-  dessert: '甜品 Dessert',
-  drink: '饮品 Drink',
-  'home-cooking': '烹饪 Cooking',
-  'semi-finished': '速食 FastFood',
-  'soup': '汤 Soup',
-  'staple': '主食 Staple',
-  aquatic: '水产 Aquatic Food',
-  'vegetable_dish': '素菜 Vegetable',
-  'meat_dish': '荤菜 Meat'
+  breakfast: '早餐',
+  condiment: '佐料',
+  dessert: '甜品',
+  drink: '饮品',
+  'home-cooking': '烹饪',
+  'semi-finished': '速食',
+  'soup': '汤',
+  'staple': '主食',
+  aquatic: '水产',
+  'vegetable_dish': '素菜',
+  'meat_dish': '荤菜'
 }
 
 export default {
